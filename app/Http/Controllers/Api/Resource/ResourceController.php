5badd14e69201de02426f804bb085ec5779e941b
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-07-13 22:31:10 +0300

    add

--- app/Http/Controllers/Api/Resource/ResourceController.php
+++ app/Http/Controllers/Api/Resource/ResourceController.php
@@ -41,7 +41,6 @@ class ResourceController extends Controller
             $screenShot_path .= $name;
         }
 
-
         $data = [
             'category_id' => $req->category_id,
             'name' => $req->name,
@@ -62,16 +61,34 @@ class ResourceController extends Controller
     public function editResource(Request $req)
     {
 
+        $validator = \Validator::make($req->all(), [
+            'category_id' => 'required',
+            'name' => 'required',
+            'description' => 'required',
+            'icon' => 'required',
+            'screenShot' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->response(['msg' => $validator->errors()->first(), 'status' => 'error']);
+        }
+        if( $req->screenShot){
+            $ext = $req->screenShot->extension();
+            $name = \Str::random(10) . '.' . $ext;
+            $screenShot_path = 'resources/screenShots/';
+            $req->screenShot->storeAs('public/' . $screenShot_path, $name);
+            $screenShot_path .= $name;
+        }
+
         $resource = Resource::find($req->id);
         $resource->update([
             'name' => $req->name,
             'category_id' => $req->category_id,
             'description' => $req->description,
             'icon' => $req->icon,
-            'screenShot' => $req->screenShot,
+            'screenShot' => $req->screenShot_path,
         ]);
 
-        return $this->response(['msg' => 'Resource updated successfully', 'status' => $resource]);
+        return $this->response(['msg' => 'Resource updated successfully', 'status' => 'success', 'data' => $resource]);
     }
 
     public function deleteResource(Request $req)
@@ -83,7 +100,7 @@ class ResourceController extends Controller
         if ($validator->fails()) {
             return $this->response(['msg' => $validator->errors()->first(), 'status' => 'error']);
         }
-        
+
         $resource = Resource::find($req->id);
         $resource->delete();
         return $this->response(['msg' => 'Resource deleted successfully', 'status' => 'success']);
