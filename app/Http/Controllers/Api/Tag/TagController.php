ea067bbf786fdf2d640e5781ad74da90b5163a99
zal3 <zainabamjedlatef31@gmail.com>
2022-07-16 16:06:59 +0300

    tag

---
+++ app/Http/Controllers/Api/Tag/TagController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Tag;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Traits\HelperTrait;
+
+class TagController extends Controller
+{
+
+    use HelperTrait;
+}
