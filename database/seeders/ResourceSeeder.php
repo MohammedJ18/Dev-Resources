4777cbfc5f3667b0602231d93f1cd0361f3bb710
zal3 <zainabamjedlatef31@gmail.com>
2022-07-18 23:17:20 +0300

    a

--- database/seeders/ResourceSeeder.php
+++ database/seeders/ResourceSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Resource;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use App\Models\Tag;
 
 class ResourceSeeder extends Seeder
 {
@@ -23,7 +24,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 1,
             'name' => ' html',
             'description' => 'html is a markup language for creating web pages and web applications.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'html.png',
             'state' => true,
         ]);
 
@@ -33,7 +34,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 1,
             'name' => ' css',
             'description' => 'css is a style sheet language used for describing the presentation of a document written in a markup language.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'css.png',
             'state' => true,
         ]);
 
@@ -44,7 +45,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 2,
             'name' => ' javascript',
             'description' => 'javascript is a high-level, dynamic, untyped, and interpreted programming language.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'javascript.png',
             'state' => true,
         ]);
 
@@ -55,7 +56,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 1,
             'name' => ' php',
             'description' => 'php is a server-side scripting language designed for web development.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'php.png',
             'state' => true,
         ]);
 
@@ -65,7 +66,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 1,
             'name' => ' mysql',
             'description' => 'mysql is a relational database management system (RDBMS).',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'mysql.png',
             'state' => true,
         ]);
 
@@ -76,7 +77,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 2,
             'name' => ' mongo',
             'description' => 'mongo is a document-oriented database.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'mongo.png',
             'state' => true,
         ]);
 
@@ -87,7 +88,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 3,
             'name' => ' java',
             'description' => 'java is a general-purpose computer programming language that is concurrent, class-based, object-oriented, and specifically designed to have as few implementation dependencies as possible.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'java.png',
             'state' => true,
         ]);
 
@@ -98,7 +99,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 1,
             'name' => ' c#',
             'description' => 'c# is a multi-paradigm programming language encompassing strong typing, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'c#.png',
             'state' => true,
         ]);
 
@@ -108,7 +109,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 1,
             'name' => ' c++',
             'description' => 'c++ is a general-purpose programming language. It has imperative, object-oriented and generic programming features, while also providing many software engineering features.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'c++.png',
             'state' => true,
         ]);
 
@@ -119,7 +120,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 2,
             'name' => ' javaScript',
             'description' => 'javaScript is a high-level, dynamic, untyped, and interpreted programming language.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'javaScript.png',
             'state' => true,
         ]);
 
@@ -130,7 +131,7 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 3,
             'name' => ' python',
             'description' => 'python is an interpreted, object-oriented, high-level programming language with dynamic semantics.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'python.png',
             'state' => true,
         ]);
 
@@ -140,11 +141,17 @@ class ResourceSeeder extends Seeder
             'subsection_id' => 3,
             'name' => ' ruby',
             'description' => 'ruby is a dynamic, object-oriented, high-level programming language with a syntax reminiscent of Perl.',
-            'screenShot' => 'https://assets.hongkiat.com/uploads/minimalist-dekstop-wallpapers/non-4k/preview/24.jpg?3',
+            'screenShot' => 'ruby.png',
             'state' => true,
         ]);
 
 
+        // attach tags to resources
+        $resources = Resource::all();
+        $tags = Tag::all();
+        foreach ($resources as $resource) {
+            $resource->tags()->attach($tags->random(rand(1, 2)));
+        }
 
     }
 }
